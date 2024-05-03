de0966933723ad9893655ab694a34ffeaf2991d4
fatmaaydogdu <fatmaaydogdu581@gmail.com>
2024-05-03 16:45:53 +0300

    hero section

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
-import './Home.css';
+import React from "react";
+import "./Home.css";
+import hero_banner from "../../assets/hero_banner.jpg";
+import hero_title from "../../assets/hero_title.png";
+import play_icon from "../../assets/play_icon.png";
+import info_icon from "../../assets/info_icon.png";
 
-import Navbar from '../../components/Navbar/Navbar'
+import Navbar from "../../components/Navbar/Navbar";
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img" />
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>
+            Discovering his ties to a secret ancient order, a young man living
+            in modern Istanbul embarks on a quest to save the city from an
+            immortal enemy.
+          </p>
+          <div className="hero-btns">
+            <button className="btn"> <img src={play_icon} alt="" />Play </button>
+            <button className="btn dark-btn"> <img src={info_icon} alt="" />More Info </button>
+          </div>
+        </div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;

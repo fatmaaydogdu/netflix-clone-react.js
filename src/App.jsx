5b8703c0bfc4d3f63ba6b0a33361c4f5e04fa3d8
fatmaaydogdu <fatmaaydogdu581@gmail.com>
2024-05-07 15:04:22 +0300

    login page

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
 import React from 'react';
 import Home from './pages/Home/Home';
+import Login from './pages/Login/Login';
+import { Routes, Route} from 'react-router-dom';
 
 const App = () => {
   return (
     <div>
-      <Home />
+      <Routes>
+        <Route path='/' element={<Home />}/>
+        <Route path='/login' element={<Login />}/>
+      </Routes>
+      
     </div>
   )
 }
